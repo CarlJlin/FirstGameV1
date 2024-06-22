8e5c6bc6f8e3f8b2d0ab5c34ebda376360d51496
CarlJlin <jeanlincarlpro@gmail.com>
2024-06-22 11:23:54 +0200

    Small fixes

--- game.cpp
+++ game.cpp
@@ -18,13 +18,13 @@ simulate_game(Input* input, float dt) {
 	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
 	if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
 
-	player_ddp -= player1_dp * 10.f;
+	player_1_ddp -= player_1_dp * 10.f;
 
-	player1_p = player1_p + player1_dp * dt + player_ddp * dt * dt * .5f;
-	player1_dp = player1_dp + player_ddp * dt;
+	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
+	player_1_dp = player_1_dp + player_1_ddp * dt;
 		
 	draw_rect(0, 0, 1, 1, 0xffffff);
 	
-	draw_rect(80, player1_p, 2.5, 12, 0xff0000);
+	draw_rect(80, player_1_p, 2.5, 12, 0xff0000);
 	draw_rect(-80, 0, 2.5, 12, 0xff0000);
 }
